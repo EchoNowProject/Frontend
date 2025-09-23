dfc4bd3653146c6b537719d5ce6f59bcdc6c47cf
Antonio Blazquez <antblajim@gmail.com>
2025-09-23 15:49:30 +0200

    FEAT - Se añade el fondo de la landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,20 @@
 // src/routes.js
-import Home from "../pages/Home";
+import Home from '../pages/Home';
+import LandingPage from '../pages/LandingPage/LangingPage';
 // import otros componentes
 
 const routes = [
-    {
-        path: "/",
-        element: <Home />,
-        // podrías agregar loader, errorElement, etc.
-    },
-    // etc.
+  {
+    path: '/',
+    element: <LandingPage />,
+    // podrías agregar loader, errorElement, etc.
+  },
+  {
+    path: '/home',
+    element: <Home />,
+    // podrías agregar loader, errorElement, etc.
+  },
+  // etc.
 ];
 
 export default routes;
