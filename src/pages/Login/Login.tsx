ef05a8ea4a012a6bed29d6d037a446de9c0fe13c
Antonio Blazquez <antblajim@gmail.com>
2025-10-15 19:52:02 +0200

    1 - STYLE - SE MEJORA EL LOGIN

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import Button from '@/components/UI/Button';
+import './Login.css';
 
 export default function Login() {
   return (
     <div className="h-screen">
       <div className="grid grid-cols-1 md:grid-cols-[1.3fr_1fr] h-full">
-        {/* Lado izquierdo - solo visible en pantallas md+ */}
-        <div className="md:flex items-center justify-center">
-          <p className="text-white text-3xl font-bold">Bienvenido</p>
+        {/* Lado izquierdo */}
+        <div className="flex flex-col items-center justify-start text-center px-4 py-6 mt-50">
+          {/* Imagen */}
+          <img
+            className="logoEchoNowLogin"
+            src="/assets/images/logo-blanco.png"
+            width={250}
+            alt="EchoNow"
+          />
+
+          {/* Texto */}
+          <h1 className="text-3xl md:text-4xl font-semibold text-white max-w-2xl text-start">
+            EchoNow te ayuda a comunicarte y compartir los mejores momentos con tus personas
+            favoritas.
+          </h1>
         </div>
 
         {/* Lado derecho - formulario */}
-        <div className="flex items-center justify-center p-4">
+        <div className="flex items-center justify-center p-4 h-full">
           <div className="w-full max-w-md p-6 bg-violet-600 border-2 border-violet-400 rounded-2xl shadow-lg">
-            {/* imagemn */}
-            <div className="flex justify-center">
-              <div className="inline-flex">
-                <img
-                  src="../../../../../public/assets/images/logo-blanco.png"
-                  width={100}
-                  alt="EchoNow"
-                />
-              </div>
-            </div>
-
             {/* Titulo */}
             <h1 className="mb-6 font-bold text-white text-2xl text-center">
               Iniciar Sesión en EchoNow!
             </h1>
+
             {/* Formulario */}
             <form className="flex flex-col gap-4">
               <div className="flex flex-col">
@@ -41,9 +44,10 @@ export default function Login() {
                   name="email"
                   id="email"
                   required
-                  className="p-2 rounded-lg border-3 border-violet-800 outline-none text-white"
+                  className="p-2 rounded-lg border-3 border-violet-800 outline-none text-white bg-transparent"
                 />
               </div>
+
               <div className="flex flex-col">
                 <div className="flex items-center justify-between mb-1">
                   <label htmlFor="password" className="text-white">
@@ -51,14 +55,16 @@ export default function Login() {
                       Contraseña <span className="text-sm text-red-500">*</span>
                     </b>
                   </label>
-                  <a className="text-white text-sm">¿Has olvidado la contraseña?</a>
+                  <a className="text-white cursor-pointer text-sm hover:font-bold">
+                    ¿Has olvidado la contraseña?
+                  </a>
                 </div>
                 <input
                   type="password"
                   name="password"
                   id="password"
                   required
-                  className="p-2 rounded-lg border-3 border-violet-800 outline-none text-white"
+                  className="p-2 rounded-lg border-3 border-violet-800 outline-none text-white bg-transparent"
                 />
               </div>
 
@@ -69,10 +75,10 @@ export default function Login() {
                 />
               </div>
 
-              <div className="mt-4 flex justify-between">
-                <small className="text-white">¿Necesitas una cuenta?</small>
+              <div className="mt-4 flex justify-between cursor-pointer">
+                <small className="text-white hover:font-bold">¿Necesitas una cuenta?</small>
                 <small>
-                  <a className="text-white cursor-pointer">Registrate aqui</a>
+                  <a className="text-white hover:font-bold">Regístrate aquí</a>
                 </small>
               </div>
             </form>
